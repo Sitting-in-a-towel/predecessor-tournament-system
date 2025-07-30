9148684a0d59a8eb67c5030ee934281372129381
Sitting-in-a-towel <your-github-email@example.com>
2025-07-30 14:35:49 +1000

    Disable PostgreSQL import in Discord config for production

--- backend/config/discord.js
+++ backend/config/discord.js
@@ -1,5 +1,5 @@
 const DiscordStrategy = require('passport-discord').Strategy;
-const postgresService = require('../services/postgresql');
+// const postgresService = require('../services/postgresql'); // Disabled for production
 const logger = require('../utils/logger');
 
 module.exports = function(passport) {
